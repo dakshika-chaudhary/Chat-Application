fbc1a024c99f76c6889dfbf39d525797ddd2c45e
dakshika-chaudhary <144917802+dakshika-chaudhary@users.noreply.github.com>
2025-06-16 23:34:52 +0530

    Delete frontend directory

--- frontend/app/components/Footer.js
+++
@@ -1,8 +0,0 @@
-// app/components/Footer.js
-export default function Footer() {
-    return (
-      <footer className="bg-dark text-white p-3 text-center mt-5">
-        <p>&copy; 2024 Chit Chatter. All rights reserved.</p>
-      </footer>
-    );
-  }
